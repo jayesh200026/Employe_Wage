1891ffdfb2afc81d67be54d0093d4310185f5101
jayesh200026 <jayeshkumar8660@gmail.com>
2021-08-22 15:46:25 +0530

    Added a feature to compute the monthly wage of a employe.

--- EmployeWage.java
+++ EmployeWage.java
@@ -5,6 +5,8 @@ public class EmployeWage{
 		double Wage_per_hour=20;
 		double Full_Time_hour=8;
 		double Part_Time_hour=4;
+		int working_days_per_month=20;
+		double Monthly_wage;
 		double Daily_wage;
 		int PartTime=2;
 		int FullTime=1;
@@ -15,11 +17,15 @@ public class EmployeWage{
 					break;
 			case 1:System.out.println("Employee is present and is FullTime");
         			 Daily_wage=Wage_per_hour*Full_Time_hour;
-      			System.out.println("Daily wage of a Full Time Employe is: "+String.format("%.4f",Daily_wage));
-					break;
+					 Monthly_wage=Daily_wage*working_days_per_month;
+      			 System.out.println("Daily wage of a Full Time Employe is: "+String.format("%.4f",Daily_wage));
+					 System.out.println("Monthly wage of a Full Time Employe is: "+String.format("%.4f",Monthly_wage));
+					 break;
 			case 2:System.out.println("Employee is present and is PartTime");
         			 Daily_wage=Wage_per_hour*Part_Time_hour;
+					 Monthly_wage=Daily_wage*working_days_per_month;
         			 System.out.println("Daily wage of a Part Time Employe is: "+String.format("%.4f",Daily_wage));
+					 System.out.println("Monthly wage of a part Time Employe is: "+String.format("%.4f",Monthly_wage));
 					 break;
 			default:System.out.println("Invalid input");
 		}
