33c35bfc416bb06fcaa20ea5acb17cd069206880
jayesh200026 <jayeshkumar8660@gmail.com>
2021-08-22 11:30:40 +0530

    Added feature to compute the daily wage of employe.

--- EmployeWage.java
+++ EmployeWage.java
@@ -2,6 +2,9 @@ public class EmployeWage{
  public static void main(String args[])
 	{
 	  System.out.println("Welcome to Employe Wage computation");
+		double Wage_per_hour=20;
+		double Full_day_hour=8;
+		double Daily_wage;
 		double attendance=Math.floor(Math.random()*10)%2;
 		if(attendance==1)
 		{
@@ -10,5 +13,7 @@ public class EmployeWage{
 		else{
 			System.out.println("Employee is absent");
 		}
+		Daily_wage=Wage_per_hour*Full_day_hour;
+		System.out.println("Daily wage of a Employe is: "+String.format("%.4f",Daily_wage));
 	}
 }
