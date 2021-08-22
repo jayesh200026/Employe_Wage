df6b96751b43c4549bfb1d7ce4007db424b62089
jayesh200026 <jayeshkumar8660@gmail.com>
2021-08-22 12:15:03 +0530

    Added PartTime Employe and computed his daily wage and changed some variable names

--- EmployeWage.java
+++ EmployeWage.java
@@ -3,17 +3,26 @@ public class EmployeWage{
 	{
 	  System.out.println("Welcome to Employe Wage computation");
 		double Wage_per_hour=20;
-		double Full_day_hour=8;
+		double Full_Time_hour=8;
+		double Part_Time_hour=4;
 		double Daily_wage;
-		double attendance=Math.floor(Math.random()*10)%2;
-		if(attendance==1)
+		int PartTime=2;
+		int FullTime=1;
+		double attendance=Math.floor(Math.random()*10)%3;
+		if(attendance==FullTime)
 		{
-			System.out.println("Employee is present");
+			System.out.println("Employee is present and is FullTime");
+			Daily_wage=Wage_per_hour*Full_Time_hour;
+      System.out.println("Daily wage of a Full Time Employe is: "+String.format("%.4f",Daily_wage));
+			
+		}
+		else if(attendance==PartTime){
+			System.out.println("Employee is present and is PartTime");
+			Daily_wage=Wage_per_hour*Part_Time_hour;
+			System.out.println("Daily wage of a Part Time Employe is: "+String.format("%.4f",Daily_wage));
 		}
 		else{
-			System.out.println("Employee is absent");
+		System.out.println("Employe is absent");
 		}
-		Daily_wage=Wage_per_hour*Full_day_hour;
-		System.out.println("Daily wage of a Employe is: "+String.format("%.4f",Daily_wage));
 	}
 }
