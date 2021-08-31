1b10a40523eca394d690ca06de6f6bd2fe9889be
jayesh200026 <jayeshkumar8660@gmail.com>
2021-08-31 17:02:47 +0530

    Solved the problem using OOPs concept

--- EmployeWage.java
+++ EmployeWage.java
@@ -1,32 +1,64 @@
 
+class Employe {
+	final int IS_FULL_TIME = 1;
+	final int IS_PART_TIME = 2;
+	final int wagePerHour = 20;
+	int attendance;
+	int fullDayHour;
+	int totalWage;
+	int totalWorkingDays;
+	int totalWorkingHours;
+
+	Employe() {
+		totalWorkingDays = 0;
+		totalWorkingHours = 0;
+	}
+
+	public void getAttendance() {
+		this.attendance = (int) Math.floor(Math.random() * 10) % 3;
+
+		switch (this.attendance) {
+		case IS_FULL_TIME:
+			this.fullDayHour = 8;
+			this.totalWorkingDays += 1;
+			break;
+		case IS_PART_TIME:
+			fullDayHour = 4;
+			this.totalWorkingDays += 1;
+			break;
+		default:
+			fullDayHour = 0;
+			break;
+		}
+		this.totalWorkingHours += this.fullDayHour;
+
+	}
+
+	public void getwage() {
+		int empwage = 0;
+		empwage = this.fullDayHour * wagePerHour;
+		this.totalWage += empwage;
+		System.out.println("Employee wage for day is " + empwage);
+	}
+
+}
+
 public class EmployeWage {
+
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee wage Computation program");
-		final int IS_FULL_TIME = 1;
-		final int IS_PART_TIME = 2;
-		int wagePerHour = 20, fullDayHour = 0, totalWorkingHours = 0, totalWorkingDays = 0;
-		int empwage = 0, totalwage = 0;
-		while (totalWorkingHours < 100 || totalWorkingDays < 20) {
-			int attendance = (int) Math.floor(Math.random() * 10) % 3;
-			switch (attendance) {
-			case IS_FULL_TIME:
-				fullDayHour = 8;
-				totalWorkingDays += 1;
-				break;
-			case IS_PART_TIME:
-				fullDayHour = 4;
-				totalWorkingDays += 1;
-				break;
-			default:
-				fullDayHour = 0;
-				break;
-			}
-			empwage = fullDayHour * wagePerHour;
-			totalwage += empwage;
-			totalWorkingHours += fullDayHour;
-			System.out.println("Employee wage for day is " + empwage);
+
+		Employe emp = new Employe();
+
+		int fullDayHour = 0;
+
+		while (emp.totalWorkingHours < 100 && emp.totalWorkingDays < 20) {
+
+			emp.getAttendance();
+			emp.getwage();
+
 		}
-		System.out.println("Total wage of employee: " + totalwage);
+		System.out.println("Total wage of employee: " + emp.totalWage);
 	}
 }
