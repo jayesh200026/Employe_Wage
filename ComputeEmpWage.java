dfaf3ff51d49afe048eb71b185c68f7813e5f402
jayesh200026 <jayeshkumar8660@gmail.com>
2021-09-05 11:01:13 +0530

    Added interface to solve

---
+++ ComputeEmpWage.java
@@ -0,0 +1,11 @@
+
+/**
+ * @author jayeshkumar Only declared the methods. These methods will be defined
+ *         by those classes which implemets the interface
+ */
+public interface ComputeEmpWage {
+	public void addCompanyEmpWage(String company, int wagePerHour, int maxHoursPerMonth, int maxDaysPerMonth);
+
+	public void computWage();
+
+}
