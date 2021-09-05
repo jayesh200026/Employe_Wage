55d11246a67a07aea27619c7429c92580aa98b5c
jayesh200026 <jayeshkumar8660@gmail.com>
2021-09-05 09:45:40 +0530

    Changed the uc8 to meet business standard

--- EmployeWage.java
+++ EmployeWage.java
@@ -1,28 +1,5 @@
 import java.util.Scanner;
 
-/**
- * @author jayeshkumar
- * Every company will have name,wagePerhour,maximum working hour and days of employee.
- */
-class Company {
-	String companyName;
-	int wagePerHour;
-	int maxHour;
-	int maxDays;
-
-	public void getData() {
-		Scanner r = new Scanner(System.in);
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter company name");
-		companyName = r.nextLine();
-		System.out.println("Enter wage per hour of company");
-		wagePerHour = sc.nextInt();
-		System.out.println("Enter maximum working hour of company");
-		maxHour = sc.nextInt();
-		System.out.println("Enter maximum working days of company");
-		maxDays = sc.nextInt();
-	}
-}
 
 /**
  * @author jayeshkumar
@@ -32,7 +9,7 @@ public class EmployeWage {
 	final static int IS_FULL_TIME = 1;
 	final static int IS_PART_TIME = 2;
 
-	public static void computWage(String companyName, int wagePerHour, int maxHours, int maxDays) {
+	public static int computWage(String companyName, int wagePerHour, int maxHours, int maxDays) {
 
 		int empWage, totalHours, totalDays, empHour, totalWage;
 		empWage = totalHours = totalDays = empHour = totalWage = 0;
@@ -52,29 +29,27 @@ public class EmployeWage {
 				empHour = 0;
 			}
 			totalHours += empHour;
-			empWage = wagePerHour * empHour;
-			totalWage += empWage;
+			
 
 		}
+		empWage = wagePerHour * totalHours;
+		totalWage += empWage;
+		
+		System.out.println("Total employe wage for company "+companyName+" is "+totalWage);
+		return totalWage;
 
-		display(companyName, totalWage, totalHours, totalDays);
-
-	}
-
-	private static void display(String companyName, int wage, int totalHour, int totalDay) {
-
-		System.out.println("Company Name::" + companyName);
-		System.out.println("Total Wage::" + wage);
-		System.out.println("Total working hours per month::" + totalHour);
-		System.out.println("Total Working days per month::" + totalDay);
+		
 
 	}
 
+	
 	public static void main(String[] args) {
-		Company yml = new Company();
+		
 
-		yml.getData();
-		computWage(yml.companyName, yml.wagePerHour, yml.maxHour, yml.maxDays);
+		
+		computWage("amazon",40,90,20);
+		computWage("google",50,70,18);
+		
 
 	}
 
